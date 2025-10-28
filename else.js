55a3d5a0d7aff3d9e1b98c84643c64ba589f54c2
ragavanmragavan <ragavanmragavan3058@gmail.com>
2025-10-28 10:56:43 +0530

    initial commit

---
+++ else.js
@@ -0,0 +1,16 @@
+var age=17
+if(age>17)
+{
+    console.log("eligible for vote")
+}
+else{
+    console.log("not eligible for vote")
+}
+var a=10
+if (a>=20)
+{
+    console.log("postive number")
+}
+else if(a>0){
+    console.log("negative number")
+}
